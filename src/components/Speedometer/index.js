4d2cf148dbd08dfe5fd240962b3e0b30e0855e76
shashipreetham <shashipreetham192@gmail.com>
2023-06-13 05:20:26 +0000

    help

---
+++ src/components/Speedometer/index.js
@@ -0,0 +1,53 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class Speedometer extends Component {
+  state = {count: 0}
+
+  onDecrease = () => {
+    const {count} = this.state
+
+    if (count > 0) {
+      this.setState(prevState => ({count: prevState.count - 10}))
+    }
+  }
+
+  onIncrease = () => {
+    const {count} = this.state
+
+    if (count < 200) {
+      this.setState(prevState => ({count: prevState.count + 10}))
+    }
+  }
+
+  render() {
+    const {count} = this.state
+
+    return (
+      <div className="container">
+        <h1 className="heading">SPEEDOMETER</h1>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
+          className="speedometer"
+          alt="speedometer"
+        />
+        <h1 className="speed">Speed is {count}mph</h1>
+        <p className="speed">Min limit is 0mph,max limit is 200mph</p>
+        <div>
+          <button className="accButton" type="button" onClick={this.onIncrease}>
+            Accelerate
+          </button>
+          <button
+            className="brakeButton"
+            type="button"
+            onClick={this.onDecrease}
+          >
+            Apply Brake
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+export default Speedometer
